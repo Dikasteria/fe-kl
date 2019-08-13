8e501c47954718ff47d6c45d241d3c062e305cf4
Dikasteria <rjhey@hotmail.com>
2019-08-13 18:27:55 +0100

    Add sorting buttons to Filter component

--- korelogic-front-end/src/components/Filter.jsx
+++ korelogic-front-end/src/components/Filter.jsx
@@ -6,7 +6,29 @@ class Search extends Component {
   };
 
   render() {
-    return <div>Filter By Food Here</div>;
+    return (
+      <div className="restaurant_filter">
+        <h3>Filter By Cuisine Here</h3>
+        <section className="filter_buttons">
+          <button
+            className="sort_button"
+            type="submit"
+            onClick={this.handleClick}
+            value="Argentinian"
+          >
+            Argentinian
+          </button>
+          <button>Asian</button>
+          <button>Balti</button>
+          <button>Bar</button>
+          <button>British</button>
+          <button>Cafe</button>
+          <button>International</button>
+          <button>Pub</button>
+          <button>Steakhouse</button>
+        </section>
+      </div>
+    );
   }
 }
 
